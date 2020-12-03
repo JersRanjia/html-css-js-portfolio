ba9f537375f49f420f28660e284c9e673cb54027
JersRanjia <jersrandria@yahoo.fr>
2020-12-04 00:08:48 +0300

    add popup for all projects and works

--- js/engine.js
+++ js/engine.js
@@ -12,4 +12,73 @@ function init_background() {
     }
 }
 
-init_background() ;
+// init_background() ;
+
+function init_project_clickable() {
+
+}
+
+function popup_project(idProject) {
+
+    var id_proj = "#" + idProject ;
+    var titre = document.querySelector(id_proj + " h2").innerHTML
+    var descri = document.querySelector(id_proj + " .description-project").innerHTML
+
+    var modal_fond = document.getElementById("modal-fond")
+    var modal_out = document.getElementById("modal-out")
+    var definition_project = document.getElementById("definition-project")
+    var title_project = document.getElementById("title-project")
+
+    title_project.innerHTML = titre
+    definition_project.innerHTML = descri
+
+    modal_fond.style.display = "flex"
+    modal_out.style.display = "flex"
+
+}
+
+function remove_popup_project() {
+
+    var modal_fond = document.getElementById("modal-fond")
+    var modal_out = document.getElementById("modal-out")
+
+    modal_fond.style.display = "none"
+    modal_out.style.display = "none"
+
+}
+
+// popup_work
+
+function popup_work(idWork) {
+
+    var id_work = "#" + idWork ;
+
+    var poste = document.querySelector(id_work + " .td-poste").innerHTML
+    var langage = document.querySelector(id_work + " .td-langage").innerHTML
+    var mission = document.querySelector(id_work + " .td-mission").innerHTML
+
+    var modal_fond = document.getElementById("modal-fond")
+    var modal_work_out = document.getElementById("modal-work-out")
+
+    var post_work = document.getElementById("post-work")
+    var lang_work = document.getElementById("lang-work")
+    var definition_work = document.getElementById("definition-work")
+
+    post_work.innerHTML = poste
+    lang_work.innerHTML = langage
+    definition_work.innerHTML = mission
+
+    modal_fond.style.display = "flex"
+    modal_work_out.style.display = "flex"
+
+}
+
+function remove_popup_work() {
+
+    var modal_fond = document.getElementById("modal-fond")
+    var modal_work_out = document.getElementById("modal-work-out")
+
+    modal_fond.style.display = "none"
+    modal_work_out.style.display = "none"
+
+}
